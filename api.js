595d6182c85110a39e7db27eca884c65100a8154
Baz <jennigoldstein@gmail.com>
2018-08-03 14:39:15 -0500

    update

--- api.js
+++ api.js
@@ -19,18 +19,28 @@ $(document).ready(function () {
             "begin_date": startYear,
             "end_date": endYear
         });
+
         $.ajax({
             url: url,
             method: 'GET',
         }).then(function (termResponse) {
             console.log(termResponse);
             for (i = 0; i < records; i++) {
+                var articleLi = $("<li class='list-group-item'>");
+
                 var title= (termResponse.response.docs[i].headline.main);
                 var author= (termResponse.response.docs[i].byline.original);
-                var section= (termResponse.response.docs[i].byline.original);
-                var site= (termResponse.response.docs[i].byline.original);
+                var section= (termResponse.response.docs[i].section_name);
+                var timeStamp=(termResponse.response.docs[i].pub_date);
+                var site= (termResponse.response.docs[i].web_url);
                 
-// $("#results").append
+                articleLi.append($("<p>" + title + "</p>"));
+                articleLi.append($("<p>" + author + "</p>"));
+                articleLi.append($("<p>" + section + "</p>"));
+                articleLi.append($("<p>" + timeStamp + "</p>"));
+                articleLi.append($("<p>" + site + "</p>"));
+                
+                $(".list-group").append(articleLi);
             }
         });
     });
